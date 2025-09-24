c1ffef6c8d1cb65e069cbf498496a314d440d0fa
cagla.sen <cagla.sen@tubitak.gov.tr>
2025-09-24 21:01:03 +0300

    refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,14 @@ import { RouterView } from 'vue-router'
   box-sizing: border-box;
 }
 
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
 body {
   font-family: 'Roboto', 'Google Sans', -apple-system, BlinkMacSystemFont, sans-serif;
 }
