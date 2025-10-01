0d9bc62daf4a8d49dd0b9e1db8cb504ab4658157
Salih OKUR <salih.okur@tubitak.gov.tr>
2025-10-01 20:37:20 +0300

    redirect frontende taşındı

---
+++ src/components/LoginProcess.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
+const router = useRouter()
+
+// URL'den code parametresini al
+const getQueryParam = (key) => new URLSearchParams(window.location.search).get(key)
+
+onMounted(async () => {
+  const code = getQueryParam('code')
+  if (!code) return
+
+  try {
+    const res = await axios.get(`http://localhost:8080/auth/callback?code=${code}`, { withCredentials: true })
+
+    if (res.data?.jwt) {
+      // JWT ve user bilgilerini kaydet
+      localStorage.setItem('jwt', res.data.jwt)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
+
+      // Frontend yönlendirme
+      router.push('/upload/123')
+    }
+  } catch (err) {
+    console.error('Login callback hatası:', err)
+  }
+})
+</script>
+
+<template>
+  <div>
+    Giriş yapılıyor...
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/LoginPage.vue'
 import WeddingMemoriesView from '@/views/WeddingMemoriesView.vue'
+import LoginProcess from '@/components/LoginProcess.vue'
 
 const routes = [
   { path: '/login', name: 'Login', component: LoginPage },
   { path: '/upload/:id', name: 'UploadWeddingMemories', component: WeddingMemoriesView },
-  { path: '/show/:id', name: 'ShowWeddingMemories', component: WeddingMemoriesView }
+  { path: '/show/:id', name: 'ShowWeddingMemories', component: WeddingMemoriesView },
+  { path: '/auth/callback', component: LoginProcess }, // ← BU ÖNEMLİ
+
 ]
 
 const router = createRouter({
